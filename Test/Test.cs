f2f751cc44df8c7e035b0572975975ce791edb54
MarHelen <hmarushchenko@sift.com>
2020-05-05 16:18:13 -0700

    add Transaction event test coverage

--- Test/Test.cs
+++ Test/Test.cs
@@ -261,5 +261,43 @@ namespace Test
             Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
                          Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
         }
+
+        [Fact]
+        public void TestTransactionEvent()
+        {
+            var transaction = new Transaction
+            {
+                user_id = "test_dotnet_booking_with_all_fields",
+                amount = 1000000000000L,
+                currency_code = "USD",
+                session_id = "gigtleqddo84l8cm15qe4il",
+                transaction_type = "$sale",
+                transaction_status = "$failure",
+                decline_category = "$invalid"
+            };
+
+            // Augment with custom fields
+            transaction.AddField("foo", "bar");
+            Assert.Equal("{\"$type\":\"$transaction\",\"$user_id\":\"test_dotnet_booking_with_all_fields\",\"$session_id\":\"gigtleqddo84l8cm15qe4il\"," +
+                                 "\"$transaction_type\":\"$sale\",\"$transaction_status\":\"$failure\",\"$amount\":1000000000000,\"$currency_code\":\"USD\"," +
+                                 "\"$decline_category\":\"$invalid\",\"foo\":\"bar\"}", transaction.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = transaction
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = transaction,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
+                          Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
+        }
     }
 }
