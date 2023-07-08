81f193f5c25da079c8bda7aa465a75daad9a5e6a
binish-exalture <binishb@exalture.com>
2023-07-08 20:53:06 +0530

    added unit test- TestVerificationEvent

--- Test/Test.cs
+++ Test/Test.cs
@@ -1316,7 +1316,7 @@ namespace Test
 
             Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
                           Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
-        } 
+        }
 
         [Fact]
         public void TestScorePercentile()
@@ -1342,6 +1342,52 @@ namespace Test
             };
 
             Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&fields=SCORE_PERCENTILES&return_score=true",
+                          Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
+        }
+
+        [Fact]
+        public void TestVerificationEvent()
+        {
+            var sessionId = "sessionId";
+            var verification = new Verification
+            {
+                user_id = "billy_jones_301",
+                session_id = sessionId,
+                status = "$pending",
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-GB",
+                    content_language = "en-US"
+                },
+                verified_event = "$login",
+                verified_entity_id = "123",
+                verification_type = "$sms",
+                verified_value = "14155551212",
+                reason = "$user_setting",
+                brand_name = "xyz",
+                site_country = "AU",
+                site_domain = "somehost.example.com"
+            };
+
+            Assert.Equal("{\"$type\":\"$verification\",\"$user_id\":\"billy_jones_301\",\"$session_id\":\"sessionId\",\"$status\":\"$pending\",\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-GB\",\"$content_language\":\"en-US\"},\"$verified_event\":\"$login\",\"$verified_entity_id\":\"123\",\"$verification_type\":\"$sms\",\"$verified_value\":\"14155551212\",\"$reason\":\"$user_setting\",\"$brand_name\":\"xyz\",\"$site_country\":\"AU\",\"$site_domain\":\"somehost.example.com\"}",
+                                 verification.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = verification
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = verification,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
                           Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
         }
     }
