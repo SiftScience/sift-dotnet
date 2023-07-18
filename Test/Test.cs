1716cb90de7577bdd440d8f7750a601905b0cf09
binish-exalture <binishb@exalture.com>
2023-07-18 16:58:07 +0530

    unit test- TestUpdateContentReview completed

--- Test/Test.cs
+++ Test/Test.cs
@@ -1550,13 +1550,13 @@ namespace Test
                 content_id = "message-23412",
                 session_id = "a234ksjfgn435sfg",
                 status = "$active",
-                ip = "255.255.255.0",                
+                ip = "255.255.255.0",
                 message = new Message()
                 {
                     body = "Let’s meet at 5pm",
                     contact_email = "alex_301@domain.com",
                     root_content_id = "listing-123",
-                    recipient_user_ids = new ObservableCollection<string>() { "fy9h989sjphh71"},
+                    recipient_user_ids = new ObservableCollection<string>() { "fy9h989sjphh71" },
                     images = new ObservableCollection<Image>()
                     {
                         new Image()
@@ -1760,6 +1760,108 @@ namespace Test
             string updateProfileBody = "{\"$type\":\"$update_content\",\"$user_id\":\"fyw3989sjpqr71\",\"$content_id\":\"listing-23412\",\"$session_id\":\"a234ksjfgn435sfg\",\"$status\":\"$active\",\"$ip\":\"255.255.255.0\",\"$profile\":{\"$body\":\"Hi! My name is Alex and I just moved to New London!\",\"$contact_email\":\"alex_301@domain.com\",\"$contact_address\":{\"$name\":\"Alex Smith\",\"$address_1\":\"abc\",\"$address_2\":\"xyz\",\"$city\":\"New London\",\"$region\":\"New Hampshire\",\"$country\":\"US\",\"$zipcode\":\"03257\",\"$phone\":\"1-415-555-6041\"},\"$images\":[{\"$md5_hash\":\"0cc175b9c0f1b6a831c399e269772661\",\"$link\":\"https://www.domain.com/file.png\",\"$description\":\"Alex’s picture\"},{\"$md5_hash\":\"0cc175b9c0f1b6a831c399e269772661\"}],\"$categories\":[\"Friends\",\"Long-term dating\"]},\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$brand_name\":\"sift\",\"$site_country\":\"US\",\"$site_domain\":\"sift.com\"}";
 
             Assert.Equal(updateProfileBody, updateContent.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = updateContent
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = updateContent,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
+                          Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
+        }
+
+        //UpdateContentReview
+        [Fact]
+        public void TestUpdateContentReview()
+        {
+            var sessionId = "sessionId";
+            var updateContent = new UpdateContent
+            {
+                user_id = "fyw3989sjpqr71",
+                content_id = "review-23412",
+                session_id = "a234ksjfgn435sfg",
+                status = "$active",
+                ip = "255.255.255.0",
+                review = new Review()
+                {
+                    subject = "Amazing Tacos!",
+                    body = "I ate the tacos.",
+                    contact_email = "alex_301@domain.com",
+                    locations = new ObservableCollection<Address>()
+                    {
+                        new Address()
+                        {
+                            name = "Bill Jones",
+                            address_1 = "abc",
+                            address_2 = "xyz",
+                            city = "Seattle",
+                            region = "Washington",
+                            country = "US",
+                            zipcode = "98112",
+                            phone = "1-415-555-6041"
+                        },
+                        new Address()
+                        {
+                            name = "Bill Jones"
+                        }
+
+                    },
+                    item_reviewed = new Item()
+                    {
+                        item_id = "B004834GQO",
+                        product_title = "The Slanket Blanket-Texas Tea",
+                        price = 39990000,
+                        currency_code = "USD",
+                        upc = "6786211451001",
+                        sku = "004834GQ",
+                        isbn = "0446576220",
+                        brand = "Slanket",
+                        manufacturer = "Slanket",
+                        category = "Blankets & Throws",
+                        tags = new ObservableCollection<string>() { "Awesome", "Wintertime specials" },
+                        color = "Texas Tea",
+                        size = "6",
+                    },
+
+                    reviewed_content_id = "listing-234234",
+                    rating = 4.5,
+                    images = new ObservableCollection<Image>()
+                    {
+                        new Image()
+                        {
+                            md5_hash = "0cc175b9c0f1b6a831c399e269772661",
+                            link = "https://www.domain.com/file.png",
+                            description = "Calamari tacos."
+                        },
+                        new Image()
+                        {
+                            md5_hash = "0cc175b9c0f1b6a831c399e269772661"
+                        }
+                    },
+                },
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_country = "US",
+                site_domain = "sift.com",
+            };
+
+            string updateReviewBody = "{\"$type\":\"$update_content\",\"$user_id\":\"fyw3989sjpqr71\",\"$content_id\":\"review-23412\",\"$session_id\":\"a234ksjfgn435sfg\",\"$status\":\"$active\",\"$ip\":\"255.255.255.0\",\"$review\":{\"$subject\":\"Amazing Tacos!\",\"$body\":\"I ate the tacos.\",\"$contact_email\":\"alex_301@domain.com\",\"$locations\":[{\"$name\":\"Bill Jones\",\"$address_1\":\"abc\",\"$address_2\":\"xyz\",\"$city\":\"Seattle\",\"$region\":\"Washington\",\"$country\":\"US\",\"$zipcode\":\"98112\",\"$phone\":\"1-415-555-6041\"},{\"$name\":\"Bill Jones\"}],\"$item_reviewed\":{\"$item_id\":\"B004834GQO\",\"$product_title\":\"The Slanket Blanket-Texas Tea\",\"$price\":39990000,\"$currency_code\":\"USD\",\"$upc\":\"6786211451001\",\"$sku\":\"004834GQ\",\"$isbn\":\"0446576220\",\"$brand\":\"Slanket\",\"$manufacturer\":\"Slanket\",\"$category\":\"Blankets & Throws\",\"$tags\":[\"Awesome\",\"Wintertime specials\"],\"$color\":\"Texas Tea\",\"$size\":\"6\"},\"$reviewed_content_id\":\"listing-234234\",\"$rating\":4.5,\"$images\":[{\"$md5_hash\":\"0cc175b9c0f1b6a831c399e269772661\",\"$link\":\"https://www.domain.com/file.png\",\"$description\":\"Calamari tacos.\"},{\"$md5_hash\":\"0cc175b9c0f1b6a831c399e269772661\"}]},\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$brand_name\":\"sift\",\"$site_country\":\"US\",\"$site_domain\":\"sift.com\"}";
+
+            Assert.Equal(updateReviewBody, updateContent.ToJson());
 
             EventRequest eventRequest = new EventRequest
             {
