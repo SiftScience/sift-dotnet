e06829d98eb0aba2e234622cbae9cc820f60f0e4
binish-exalture <binishb@exalture.com>
2023-07-19 18:13:14 +0530

    UnitTest-AdditemtocartEvent added

--- Test/Test.cs
+++ Test/Test.cs
@@ -1344,5 +1344,65 @@ namespace Test
             Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&fields=SCORE_PERCENTILES&return_score=true",
                           Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
         }
+
+        [Fact]
+        public void TestAdditemtocartEvent()
+        {
+            var sessionId = "sessionId";
+            var addItemToCart = new AddItemToCart
+            {
+                user_id = "billy_jones_301",
+                session_id = "gigtleqddo84l8cm15qe4il",
+                item = new Item()
+                {
+                    item_id = "B004834GQO",
+                    product_title = "The Slanket Blanket-Texas Tea",
+                    price = 39990000,
+                    currency_code = "USD",
+                    upc = "6786211451001",
+                    sku = "004834GQ",
+                    isbn = "0446576220",
+                    brand = "Slanket",
+                    manufacturer = "Slanket",
+                    category = "Blankets & Throws",
+                    tags = new ObservableCollection<string>() { "Awesome", "Wintertime specials" },
+                    color = "Texas Tea",
+                    quantity = 16,
+                },
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },                
+                brand_name = "sift",
+                site_country = "US",
+                site_domain = "sift.com",
+                user_email = "billjones1@example.com",
+                verification_phone_number = "+123456789012"
+            };
+
+            string addItemToCartBody = "{\"$type\":\"$add_item_to_cart\",\"$user_id\":\"billy_jones_301\",\"$session_id\":\"gigtleqddo84l8cm15qe4il\",\"$item\":{\"$item_id\":\"B004834GQO\",\"$product_title\":\"The Slanket Blanket-Texas Tea\",\"$price\":39990000,\"$currency_code\":\"USD\",\"$quantity\":16,\"$upc\":\"6786211451001\",\"$sku\":\"004834GQ\",\"$isbn\":\"0446576220\",\"$brand\":\"Slanket\",\"$manufacturer\":\"Slanket\",\"$category\":\"Blankets & Throws\",\"$tags\":[\"Awesome\",\"Wintertime specials\"],\"$color\":\"Texas Tea\"},\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$brand_name\":\"sift\",\"$site_country\":\"US\",\"$site_domain\":\"sift.com\",\"$user_email\":\"billjones1@example.com\",\"$verification_phone_number\":\"+123456789012\"}";
+
+            Assert.Equal(addItemToCartBody, addItemToCart.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = addItemToCart
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = addItemToCart,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
+                          Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
+        }
+
     }
 }
