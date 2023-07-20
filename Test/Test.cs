e7a1bf8f73820718efdd6031bc974f5e0adc69c5
binish-exalture <binishb@exalture.com>
2023-07-20 20:54:53 +0530

    Unit Test AddPromotionEvent added

--- Test/Test.cs
+++ Test/Test.cs
@@ -1404,5 +1404,75 @@ namespace Test
                           Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
         }
 
+        [Fact]
+        public void TestAddPromotionEvent()
+        {
+            var sessionId = "sessionId";
+            var addPromotion = new AddPromotion
+            {
+                user_id = "billy_jones_301",
+                session_id = "gigtleqddo84l8cm15qe4il",
+                promotions = new ObservableCollection<Promotion>()
+                {
+                    new Promotion()
+                    {
+                          promotion_id = "NewCustomerReferral2016",
+                          status = "$success",
+                          failure_reason = "$already_used",
+                          description =   "$5 off your first 5 rides",
+                          referrer_user_id = "elon-m93903",
+                          discount = new Discount()
+                          {
+                                  percentage_off = 0.2,
+                                  amount = 5000000,
+                                  currency_code = "USD",
+                                  minimum_purchase_amount = 50000000
+                          },
+                          credit_point = new CreditPoint()
+                          {
+                                  amount = 5000,
+                                  credit_point_type = "character xp points"
+                          }
+                    },
+                    new Promotion()
+                    {
+                        promotion_id = "NewCustomerReferral2016"
+                    }
+                },
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_country = "US",
+                site_domain = "sift.com",
+                user_email = "billjones1@example.com",
+                verification_phone_number = "+123456789012"
+            };
+
+            string addPromotionbody = "{\"$type\":\"$add_promotion\",\"$user_id\":\"billy_jones_301\",\"$session_id\":\"gigtleqddo84l8cm15qe4il\",\"$promotions\":[{\"$promotion_id\":\"NewCustomerReferral2016\",\"$status\":\"$success\",\"$failure_reason\":\"$already_used\",\"$description\":\"$5 off your first 5 rides\",\"$referrer_user_id\":\"elon-m93903\",\"$discount\":{\"$percentage_off\":0.2,\"$amount\":5000000,\"$currency_code\":\"USD\",\"$minimum_purchase_amount\":50000000},\"$credit_point\":{\"$amount\":5000,\"$credit_point_type\":\"character xp points\"}},{\"$promotion_id\":\"NewCustomerReferral2016\"}],\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$brand_name\":\"sift\",\"$site_country\":\"US\",\"$site_domain\":\"sift.com\",\"$user_email\":\"billjones1@example.com\",\"$verification_phone_number\":\"+123456789012\"}";
+
+            Assert.Equal(addPromotionbody, addPromotion.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = addPromotion
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = addPromotion,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
+                          Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
+        }
+
     }
 }
