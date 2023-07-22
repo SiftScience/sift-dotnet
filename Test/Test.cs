e3f01a19f302d41bb8a839e788805fb5d945e3bd
binish-exalture <binishb@exalture.com>
2023-07-23 01:27:19 +0530

    Unit Test CreateOrderEvent added

--- Test/Test.cs
+++ Test/Test.cs
@@ -1316,7 +1316,7 @@ namespace Test
 
             Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
                           Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
-        } 
+        }
 
         [Fact]
         public void TestScorePercentile()
@@ -1374,7 +1374,7 @@ namespace Test
                     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
                     accept_language = "en-US",
                     content_language = "en-GB"
-                },                
+                },
                 brand_name = "sift",
                 site_country = "US",
                 site_domain = "sift.com",
@@ -1483,7 +1483,7 @@ namespace Test
                 user_id = "billy_jones_301",
                 session_id = "gigtleqddo84l8cm15qe4il",
                 content_id = "9671500641",
-                status = "$paused",                
+                status = "$paused",
                 browser = new Browser
                 {
                     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
@@ -1621,5 +1621,147 @@ namespace Test
                           Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
         }
 
+        [Fact]
+        public void TestCreateOrderEvent()
+        {
+            var sessionId = "sessionId";
+            var createOrder = new CreateOrder
+            {
+                user_id = "billy_jones_301",
+                session_id = "gigtleqddo84l8cm15qe4il",
+                order_id = "ORDER-28168441",
+                user_email = "billjones1@example.com",
+                verification_phone_number = "+123456789012",
+                amount = 115940000,
+                currency_code = "USD",
+                billing_address = new Address()
+                {
+                    name = "Bill Jones",
+                    phone = "1-415-555-6041",
+                    address_1 = "2100 Main Street",
+                    address_2 = "Apt 3B",
+                    city = "New London",
+                    region = "New Hampshire",
+                    country = "US",
+                    zipcode = "03257"
+                },
+                payment_methods = new ObservableCollection<PaymentMethod>()
+                {
+                    new PaymentMethod()
+                    {
+                        payment_type = "$credit_card",
+                        payment_gateway = "$braintree",
+                        card_bin = "542486",
+                        card_last4 = "4444"
+                    },
+                    new PaymentMethod()
+                    {
+                        payment_type = "$credit_card"
+                    }
+
+                },
+                ordered_from = new OrderedFrom()
+                {
+                    store_id = "123",
+                    store_address = new Address()
+                    {
+                        name = "Bill Jones",
+                        phone = "1-415-555-6040",
+                        address_1 = "2100 Main Street",
+                        address_2 = "Apt 3B",
+                        city = "New London",
+                        region = "New Hampshire",
+                        country = "US",
+                        zipcode = "03257"
+                    }
+                    
+                },
+                brand_name = "sift",
+                site_domain = "sift.com",
+                site_country = "US",
+                shipping_address = new Address()
+                {
+                    name = "Bill Jones",
+                    phone = "1-415-555-6041",
+                    address_1 = "2100 Main Street",
+                    address_2 = "Apt 3B",
+                    city = "New London",
+                    region = "New Hampshire",
+                    country = "US",
+                    zipcode = "03257"
+                },
+                expedited_shipping = true,
+                shipping_method = "$physical",
+                shipping_carrier = "UPS",
+                shipping_tracking_numbers = new ObservableCollection<string>() { "1Z204E380338943508", "1Z204E380338943509" },
+                items = new ObservableCollection<Item>()
+                {
+                    new Item()
+                    {
+                        item_id = "12344321",
+                        product_title = "Microwavable Kettle Corn: Original Flavor",
+                        price = 4990000,
+                        currency_code = "USD",
+                        upc = "097564307560",
+                        sku = "03586005",
+                        isbn = "0446576220",
+                        brand = "Peters Kettle Corn",
+                        manufacturer = "Peters Kettle Corn",
+                        category = "Food and Grocery",
+                        tags = new ObservableCollection<string>() { "Popcorn", "Snacks", "On Sale" },
+                        color = "Texas Tea",
+                        quantity = 4
+                    },
+                    new Item()
+                    {
+                        item_id = "12344321"
+                    }
+                },
+                seller_user_id = "slinkys_emporium",
+                promotions = new ObservableCollection<Promotion>()
+                {
+                    new Promotion()
+                    {
+                        promotion_id = "FirstTimeBuyer",
+                        status = "$success",
+                        description = "$5 off",
+                        discount = new Discount()
+                        {
+                            amount = 5000000,
+                            currency_code = "USD",
+                            minimum_purchase_amount = 25000000
+                        }
+                    }
+                },
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                }
+            };
+            string createorderbody = "{\"$type\":\"$create_order\",\"$user_id\":\"billy_jones_301\",\"$session_id\":\"gigtleqddo84l8cm15qe4il\",\"$order_id\":\"ORDER-28168441\",\"$user_email\":\"billjones1@example.com\",\"$amount\":115940000,\"$currency_code\":\"USD\",\"$billing_address\":{\"$name\":\"Bill Jones\",\"$address_1\":\"2100 Main Street\",\"$address_2\":\"Apt 3B\",\"$city\":\"New London\",\"$region\":\"New Hampshire\",\"$country\":\"US\",\"$zipcode\":\"03257\",\"$phone\":\"1-415-555-6041\"},\"$payment_methods\":[{\"$payment_type\":\"$credit_card\",\"$payment_gateway\":\"$braintree\",\"$card_bin\":\"542486\",\"$card_last4\":\"4444\"},{\"$payment_type\":\"$credit_card\"}],\"$shipping_address\":{\"$name\":\"Bill Jones\",\"$address_1\":\"2100 Main Street\",\"$address_2\":\"Apt 3B\",\"$city\":\"New London\",\"$region\":\"New Hampshire\",\"$country\":\"US\",\"$zipcode\":\"03257\",\"$phone\":\"1-415-555-6041\"},\"$expedited_shipping\":true,\"$items\":[{\"$item_id\":\"12344321\",\"$product_title\":\"Microwavable Kettle Corn: Original Flavor\",\"$price\":4990000,\"$currency_code\":\"USD\",\"$quantity\":4,\"$upc\":\"097564307560\",\"$sku\":\"03586005\",\"$isbn\":\"0446576220\",\"$brand\":\"Peters Kettle Corn\",\"$manufacturer\":\"Peters Kettle Corn\",\"$category\":\"Food and Grocery\",\"$tags\":[\"Popcorn\",\"Snacks\",\"On Sale\"],\"$color\":\"Texas Tea\"},{\"$item_id\":\"12344321\"}],\"$seller_user_id\":\"slinkys_emporium\",\"$promotions\":[{\"$promotion_id\":\"FirstTimeBuyer\",\"$status\":\"$success\",\"$description\":\"$5 off\",\"$discount\":{\"$amount\":5000000,\"$currency_code\":\"USD\",\"$minimum_purchase_amount\":25000000}}],\"$shipping_method\":\"$physical\",\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$brand_name\":\"sift\",\"$site_country\":\"US\",\"$site_domain\":\"sift.com\",\"$ordered_from\":{\"$store_id\":\"123\",\"$store_address\":{\"$name\":\"Bill Jones\",\"$address_1\":\"2100 Main Street\",\"$address_2\":\"Apt 3B\",\"$city\":\"New London\",\"$region\":\"New Hampshire\",\"$country\":\"US\",\"$zipcode\":\"03257\",\"$phone\":\"1-415-555-6040\"}},\"$verification_phone_number\":\"+123456789012\",\"$shipping_carrier\":\"UPS\",\"$shipping_tracking_numbers\":[\"1Z204E380338943508\",\"1Z204E380338943509\"]}";
+
+            Assert.Equal(createorderbody, createOrder.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = createOrder
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = createOrder,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
+                            Uri.UnescapeDataString(eventRequest.Request.RequestUri.ToString()));
+        }
+
     }
+
 }
