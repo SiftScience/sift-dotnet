5dba2bb00a4293bf4261d0592a91d1af489bf5d3
binish-exalture <binishb@exalture.com>
2023-08-15 10:59:15 +0530

    IntegrationTest Chargeback event added

--- Test.Integration.Net7/UnitTest1.cs
+++ Test.Integration.Net7/UnitTest1.cs
@@ -124,6 +124,51 @@ namespace Test.Integration.Net7
             }
         }
 
+        [Fact]
+        public void IntegrationTest_Chargeback()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            var sessionId = "sessionId";
+            var chargeback = new Chargeback
+            {
+                user_id = "billy_jones_301",
+                order_id = "ORDER-123124124",
+                transaction_id = "719637215",
+                session_id = "gigtleqddo84l8cm15qe4il",
+                chargeback_state = "$lost",
+                chargeback_reason = "$duplicate",
+                merchant_profile = new MerchantProfile()
+                {
+                    merchant_id = "AX527123",
+                    merchant_category_code = "1234",
+                    merchant_name = "Dream Company",
+                    merchant_address = new Address()
+                    {
+                        phone = "1-415-555-6040",
+                        address_1 = "2100 Main Street",
+                        address_2 = "Apt 3B",
+                        city = "New London",
+                        region = "New Hampshire",
+                        country = "US",
+                        zipcode = "03257"
+
+                    }
+                }
+            };
+
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = chargeback
+            };
+            try
+            {
+                EventResponse res = sift.SendAsync(eventRequest).Result;
+            }
+            catch (AggregateException ae)
+            {
+                // Handle InnerException
+            }
+        }
 
     }
 }
