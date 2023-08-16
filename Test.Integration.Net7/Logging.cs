87e1665ec2ad9636e29572bf9804cd5c32f97751
binish-exalture <binishb@exalture.com>
2023-08-16 21:31:03 +0530

    added integration test for Logout

--- Test.Integration.Net7/Logging.cs
+++ Test.Integration.Net7/Logging.cs
@@ -39,5 +39,31 @@ namespace Test
             EventResponse res = sift.SendAsync(eventRequest).Result;
             Assert.Equal("OK", res.ErrorMessage);
         }
+
+        [Fact]
+        public void IntegrationTest_Logout()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            var sessionId = "sessionId";
+            var logout = new Logout
+            {
+                user_id = "billy_jones_301",                
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_domain = "sift.com",
+                site_country = "US"
+            };
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = logout
+            };
+            EventResponse res = sift.SendAsync(eventRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+        }
     }
 }
