95b3f4e7e8c352d31b9b6e12cbaf918161dd7a9b
binish-exalture <binishb@exalture.com>
2023-08-17 00:42:14 +0530

    added integration test for chargeback

--- Test.Integration.Net7/Chargebacks.cs
+++ Test.Integration.Net7/Chargebacks.cs
@@ -1,61 +1,43 @@
 using Sift;
-using System.Collections.ObjectModel;
 using Xunit;
 
 namespace Test
 {
-    public class Promotions
+    public class Chargebacks
     {
         [Fact]
-        public void IntegrationTest_AddPromotion()
+        public void IntegrationTest_Chargeback()
         {
             var sift = new Client("ccd68efbe25809bc");
             var sessionId = "sessionId";
-            var addPromotion = new AddPromotion
+            var chargeback = new Chargeback
             {
                 user_id = "billy_jones_301",
-                session_id = "gigtleqddo84l8cm15qe4il",
-                promotions = new ObservableCollection<Promotion>()
+                order_id = "ORDER-123124124",
+                transaction_id = "719637215",
+                chargeback_state = "$lost",
+                chargeback_reason = "$duplicate",
+                merchant_profile = new MerchantProfile()
                 {
-                    new Promotion()
+                    merchant_id = "AX527123",
+                    merchant_category_code = "1234",
+                    merchant_name = "Dream Company",
+                    merchant_address = new Address()
                     {
-                          promotion_id = "NewCustomerReferral2016",
-                          status = "$success",
-                          failure_reason = "$already_used",
-                          description =   "$5 off your first 5 rides",
-                          referrer_user_id = "elon-m93903",
-                          discount = new Discount()
-                          {
-                                  percentage_off = 0.2,
-                                  amount = 5000000,
-                                  currency_code = "USD",
-                                  minimum_purchase_amount = 50000000
-                          },
-                          credit_point = new CreditPoint()
-                          {
-                                  amount = 5000,
-                                  credit_point_type = "character xp points"
-                          }
-                    },
-                    new Promotion()
-                    {
-                        promotion_id = "NewCustomerReferral2016"
+                        phone = "1-415-555-6040",
+                        address_1 = "2100 Main Street",
+                        address_2 = "Apt 3B",
+                        city = "New London",
+                        region = "New Hampshire",
+                        country = "US",
+                        zipcode = "03257"
+
                     }
-                },
-                browser = new Browser
-                {
-                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-                    accept_language = "en-US",
-                    content_language = "en-GB"
-                },
-                brand_name = "sift",
-                site_country = "US",
-                site_domain = "sift.com",
-                verification_phone_number = "+123456789012"
+                }
             };
             EventRequest eventRequest = new EventRequest()
             {
-                Event = addPromotion
+                Event = chargeback
             };
             EventResponse res = sift.SendAsync(eventRequest).Result;
             Assert.Equal("OK", res.ErrorMessage);
