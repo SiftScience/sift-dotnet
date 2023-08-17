384cca72e60ca6947da6df842471d04b94c53a2f
binish-exalture <binishb@exalture.com>
2023-08-17 12:37:58 +0530

    added integration test for SecurityNotification

--- Test.Integration.Net7/Notifications.cs
+++ Test.Integration.Net7/Notifications.cs
@@ -3,21 +3,33 @@ using Xunit;
 
 namespace Test
 {
-    public class Sessions
+    public class Notifications
     {
         [Fact]
-        public void IntegrationTest_LinkSessionToUser()
+        public void IntegrationTest_SecurityNotification()
         {
             var sift = new Client("ccd68efbe25809bc");
             var sessionId = "sessionId";
-            var linkSessionToUser = new LinkSessionToUser
+            var securityNotification = new SecurityNotification
             {
                 user_id = "billy_jones_301",
-                session_id = "gigtleqddo84l8cm15qe4il"
+                session_id = "gigtleqddo84l8cm15qe4il",
+                notification_type = "$email",
+                notified_value = "billy123@domain.com",
+                notification_status = "$sent",
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_domain = "sift.com",
+                site_country = "US"
             };
             EventRequest eventRequest = new EventRequest()
             {
-                Event = linkSessionToUser
+                Event = securityNotification
             };
             EventResponse res = sift.SendAsync(eventRequest).Result;
             Assert.Equal("OK", res.ErrorMessage);
