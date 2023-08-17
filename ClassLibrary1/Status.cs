d4abbdf5686b1738ed04d2f5d2e5b10db0b5d70b
binish-exalture <binishb@exalture.com>
2023-08-18 02:53:27 +0530

    added IntegrationTest for ContentStatus - fw48

---
+++ ClassLibrary1/Status.cs
@@ -0,0 +1,39 @@
+﻿using Sift;
+using Xunit;
+
+namespace Test
+{
+    public class Status
+    {
+        [Fact]
+        public void IntegrationTest_ContentStatus()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            var sessionId = "sessionId";
+            var contentStatus = new ContentStatus
+            {
+                user_id = "billy_jones_301",
+                session_id = "gigtleqddo84l8cm15qe4il",
+                content_id = "9671500641",
+                status = "$paused",
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_country = "US",
+                site_domain = "sift.com",
+                verification_phone_number = "+123456789012"
+            };
+
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = contentStatus
+            };
+            EventResponse res = sift.SendAsync(eventRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+        }
+    }
+}
