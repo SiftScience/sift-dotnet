0ab98acbe6bfe400b31ceb95fc787cb660731f25
binish-exalture <binishb@exalture.com>
2023-08-18 02:21:26 +0530

    added integration test for CreateContentComment - fw48

--- ClassLibrary1/CreateContents.cs
+++ ClassLibrary1/CreateContents.cs
@@ -1,10 +1,61 @@
 ﻿using Sift;
+using System.Collections.ObjectModel;
 using Xunit;
 
 namespace Test
 {
     public class CreateContents
     {
-        
+        [Fact]
+        public void IntegrationTest_CreateContentComment()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            var sessionId = "sessionId";
+            var createContent = new CreateContent
+            {
+                user_id = "fyw3989sjpqr71",
+                content_id = "comment-23412",
+                session_id = "a234ksjfgn435sfg",
+                status = "$active",
+                ip = "255.255.255.0",
+                comment = new Comment()
+                {
+                    body = "Congrats on the new role!",
+                    contact_email = "alex_301@domain.com",
+                    parent_comment_id = "comment-23407",
+                    root_content_id = "listing-12923213",
+                    images = new ObservableCollection<Image>()
+                    {
+                        new Image()
+                        {
+                            md5_hash = "0cc175b9c0f1b6a831c399e269772661",
+                            link = "https://www.domain.com/file.png",
+                            description =   "An old picture"
+                        },
+                        new Image()
+                        {
+                            md5_hash = "0cc175b9c0f1b6a831c399e269772661"
+                        }
+                    }
+                },
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_domain = "sift.com",
+                site_country = "US"
+            };
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = createContent
+            };
+            EventResponse res = sift.SendAsync(eventRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+        }
+
+
     }
 }
