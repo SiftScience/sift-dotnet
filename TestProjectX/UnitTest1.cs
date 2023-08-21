9f8490f40f46c74b32f1371103da08820135f755
binish-exalture <binishb@exalture.com>
2023-08-21 10:57:37 +0530

    updated Test.Integration.Net7.csproj

---
+++ TestProjectX/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System.Text;
+using Xunit;
+
+namespace TestProjectX
+{
+    public class UnitTestX
+    {
+        [Fact]
+        public void TestX()
+        {
+            Assert.Equal(true,true);
+        }
+    }
+}
