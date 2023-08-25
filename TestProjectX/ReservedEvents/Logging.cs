1af3a466937e9e6f9729f8881bf1ef6452c4bf60
binish-exalture <binishb@exalture.com>
2023-08-25 20:07:53 +0530

    Added Integration Test for Logout - fw48

--- TestProjectX/ReservedEvents/Logging.cs
+++ TestProjectX/ReservedEvents/Logging.cs
@@ -39,6 +39,33 @@ namespace TestProjectX.ReservedEvents
             EventResponse res = sift.SendAsync(eventRequest).Result;
             Assert.Equal("OK", res.ErrorMessage);
             Assert.Equal("0", res.Status.ToString());
-        }        
+        }
+
+        [Fact]
+        public void IntegrationTest_Logout()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            var sessionId = "sessionId";
+            var logout = new Logout
+            {
+                user_id = "billy_jones_301",
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_domain = "sift.com",
+                site_country = "US"
+            };
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = logout
+            };
+            EventResponse res = sift.SendAsync(eventRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+            Assert.Equal("0", res.Status.ToString());
+        }
     }
 }
