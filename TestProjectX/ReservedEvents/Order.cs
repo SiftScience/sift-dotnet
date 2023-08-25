e13acdbd1d7486b5614352a99aa7663f1d1787c6
binish-exalture <binishb@exalture.com>
2023-08-25 20:21:35 +0530

    Added Integration Test for UpdateOrder - fw48

--- TestProjectX/ReservedEvents/Order.cs
+++ TestProjectX/ReservedEvents/Order.cs
@@ -130,6 +130,134 @@ namespace TestProjectX.ReservedEvents
             EventResponse res = sift.SendAsync(eventRequest).Result;
             Assert.Equal("OK", res.ErrorMessage);
             Assert.Equal("0", res.Status.ToString());
-        }        
+        }
+
+        [Fact]
+        public void IntegrationTest_UpdateOrder()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            var sessionId = "sessionId";
+            var updateOrder = new UpdateOrder
+            {
+                user_id = "billy_jones_301",
+                session_id = "gigtleqddo84l8cm15qe4il",
+                order_id = "ORDER-28168441",
+                user_email = "billjones1@example.com",
+                verification_phone_number = "+123456789012",
+                amount = 115940000,
+                currency_code = "USD",
+                billing_address = new Address()
+                {
+                    name = "Bill Jones",
+                    phone = "1-415-555-6041",
+                    address_1 = "2100 Main Street",
+                    address_2 = "Apt 3B",
+                    city = "New London",
+                    region = "New Hampshire",
+                    country = "US",
+                    zipcode = "03257"
+                },
+                payment_methods = new ObservableCollection<PaymentMethod>()
+                {
+                    new PaymentMethod()
+                    {
+                        payment_type = "$credit_card",
+                        payment_gateway = "$braintree",
+                        card_bin = "542486",
+                        card_last4 = "4444"
+                    },
+                    new PaymentMethod()
+                    {
+                        payment_type = "$credit_card"
+                    }
+
+                },
+                ordered_from = new OrderedFrom()
+                {
+                    store_id = "123",
+                    store_address = new Address()
+                    {
+                        name = "Bill Jones",
+                        phone = "1-415-555-6040",
+                        address_1 = "2100 Main Street",
+                        address_2 = "Apt 3B",
+                        city = "New London",
+                        region = "New Hampshire",
+                        country = "US",
+                        zipcode = "03257"
+                    }
+
+                },
+                brand_name = "sift",
+                site_domain = "sift.com",
+                site_country = "US",
+                shipping_address = new Address()
+                {
+                    name = "Bill Jones",
+                    phone = "1-415-555-6041",
+                    address_1 = "2100 Main Street",
+                    address_2 = "Apt 3B",
+                    city = "New London",
+                    region = "New Hampshire",
+                    country = "US",
+                    zipcode = "03257"
+                },
+                expedited_shipping = true,
+                shipping_method = "$physical",
+                items = new ObservableCollection<Item>()
+                {
+                    new Item()
+                    {
+                        item_id = "12344321",
+                        product_title = "Microwavable Kettle Corn: Original Flavor",
+                        price = 4990000,
+                        currency_code = "USD",
+                        upc = "097564307560",
+                        sku = "03586005",
+                        isbn = "0446576220",
+                        brand = "Peters Kettle Corn",
+                        manufacturer = "Peters Kettle Corn",
+                        category = "Food and Grocery",
+                        tags = new ObservableCollection<string>() { "Popcorn", "Snacks", "On Sale" },
+                        color = "Texas Tea",
+                        quantity = 4
+                    },
+                    new Item()
+                    {
+                        item_id = "12344321"
+                    }
+                },
+                seller_user_id = "slinkys_emporium",
+                promotions = new ObservableCollection<Promotion>()
+                {
+                    new Promotion()
+                    {
+                        promotion_id = "FirstTimeBuyer",
+                        status = "$success",
+                        description = "$5 off",
+                        discount = new Discount()
+                        {
+                            amount = 5000000,
+                            currency_code = "USD",
+                            minimum_purchase_amount = 25000000
+                        }
+                    }
+                },
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                }
+            };
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = updateOrder
+            };
+            EventResponse res = sift.SendAsync(eventRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+            Assert.Equal("0", res.Status.ToString());
+        }
+
     }
 }
