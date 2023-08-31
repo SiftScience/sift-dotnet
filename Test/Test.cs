66ccab854bb0c6c9f922cf7dd294027d42cd8313
binish-exalture <123974490+binish-exalture@users.noreply.github.com>
2023-08-31 13:50:00 +0530

    Msue 197 (#54)
    Support of $update_password event type

--- Test/Test.cs
+++ Test/Test.cs
@@ -2528,5 +2528,52 @@ namespace Test
             Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
                           Uri.UnescapeDataString(eventRequest.Request.RequestUri!.ToString()));
         }
+
+        [Fact]
+        public void TestUpdatePasswordEvent()
+        {
+            var updatePassword = new UpdatePassword
+            {
+                user_id = "billy_jones_301",
+                reason = "$forced_reset",
+                status = "$success",
+                session_id = "gigtleqddo84l8cm15qe4il",
+                ip = "128.148.1.135",
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },                
+                brand_name = "sift",
+                site_country = "US",
+                site_domain = "sift.com",
+                user_email = "billjones1@example.com",
+                verification_phone_number = "+123456789012"
+            };
+
+            string updatePasswordBody = "{\"$type\":\"$update_password\",\"$user_id\":\"billy_jones_301\",\"$session_id\":\"gigtleqddo84l8cm15qe4il\",\"$reason\":\"$forced_reset\",\"$status\":\"$success\",\"$ip\":\"128.148.1.135\",\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$brand_name\":\"sift\",\"$site_country\":\"US\",\"$site_domain\":\"sift.com\",\"$user_email\":\"billjones1@example.com\",\"$verification_phone_number\":\"+123456789012\"}";
+
+            Assert.Equal(updatePasswordBody, updatePassword.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = updatePassword
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri!.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = updatePassword,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
+                          Uri.UnescapeDataString(eventRequest.Request.RequestUri!.ToString()));
+        }
+
     }
+
 }
