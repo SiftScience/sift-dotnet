87747690e0e6c863058b59368dd5c1cc66f59004
binish-exalture <binishb@exalture.com>
2023-09-07 23:30:18 +0530

    Added IntegrationTest GetMerchantsRequest (.Net7)

--- Test.Integration.Net7/MerchantRequests.cs
+++ Test.Integration.Net7/MerchantRequests.cs
@@ -6,6 +6,19 @@ namespace Test.Integration.Net7.CustomEvents
 {
     public class MerchantRequests
     {
-        
+        [Fact]
+        public void IntegrationTest_GetMerchantsRequest()
+        {
+            var sift = new Client("ccd68efbe25809bc:");
+            GetMerchantsRequest getMerchantRequest = new GetMerchantsRequest
+            {
+                AccountId = "cf51f0ec-6078-46e9-a796-700af25e668c",
+                ApiKey = "ccd68efbe25809bc",
+                BatchSize = 10,
+                BatchToken = null                               
+            };
+            GetMerchantsResponse getMerchantResponse = sift.SendAsync(getMerchantRequest).Result;
+            Assert.Equal("OK", getMerchantResponse.ErrorMessage);
+        }
     }
 }
