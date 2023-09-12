3087d1c98d0abcf7a970b477504e7f243391ad55
binish-exalture <binishb@exalture.com>
2023-09-12 19:05:54 +0530

    Added Labels.cs for label related tests (.Net7)

---
+++ Test.Integration.Net7/CustomEvents/Labels.cs
@@ -0,0 +1,12 @@
+using Sift;
+using System.Collections.Generic;
+using System;
+using Xunit;
+
+namespace Test.Integration.Net7.CustomEvents
+{
+    public class LabelRequest
+    {
+        
+    }
+}
