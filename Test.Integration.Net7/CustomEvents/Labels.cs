17064dd8d611b0dddca36000c65c0f22611344ff
binish-exalture <binishb@exalture.com>
2023-09-12 23:09:31 +0530

    Added IntegrationTest UnLabelRequest - incomplete (.Net7)

--- Test.Integration.Net7/CustomEvents/Labels.cs
+++ Test.Integration.Net7/CustomEvents/Labels.cs
@@ -24,5 +24,25 @@ namespace Test.Integration.Net7.CustomEvents
             SiftResponse labelResponse = sift.SendAsync(labelRequest).Result;
             Assert.Equal("OK", labelResponse.ErrorMessage);
         }
+
+        [Fact]
+        public void IntegrationTest_UnLabelRequest()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            LabelRequest labelRequest = new LabelRequest
+            {
+                UserId = "haneeshv@exalture.com",
+                ApiKey = "ccd68efbe25809bc",
+                IsBad = true,
+                AbuseType = "payment_abuse",
+                Description = "The user was testing cards repeatedly for a valid card",
+                Source = "manual review",
+                Analyst = "someone@your-site.com"
+            };
+
+            SiftResponse labelResponse = sift.SendAsync(labelRequest).Result;
+            Assert.Equal("OK", labelResponse.ErrorMessage);
+        }
+
     }
 }
