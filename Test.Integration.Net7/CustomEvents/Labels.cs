401c2d3c74f58a044919f30bfc475d366c9db978
binish-exalture <binishb@exalture.com>
2023-09-12 19:51:39 +0530

    LabelRequest test added

--- Test.Integration.Net7/CustomEvents/Labels.cs
+++ Test.Integration.Net7/CustomEvents/Labels.cs
@@ -7,6 +7,19 @@ namespace Test.Integration.Net7.CustomEvents
 {
     public class LabelRequest
     {
-        
+        [Fact]
+        public void IntegrationTest_LabelRequest()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            LabelRequest labelRequest = new LabelRequest
+            {
+                ApiKey = "ccd68efbe25809bc",                
+                UserId = "haneeshv@exalture.com",
+                AbuseTypes = new List<string>() { "payment_abuse", "promotion_abuse" }
+            };
+
+            LabelRequest labelrequestResponse = sift.SendAsync(labelResponse).Result;
+            Assert.Equal("OK", labelrequestResponse.ErrorMessage);
+        }
     }
 }
