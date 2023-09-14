926850fea6c4f387166e0da6ca71628031d80b7c
binish-exalture <binishb@exalture.com>
2023-09-14 16:30:12 +0530

    Added IntegrationTest for GetDecisionStatusRequest (.Net7)

--- Test.Integration.Net7/CustomEvents/DecisionsRequests.cs
+++ Test.Integration.Net7/CustomEvents/DecisionsRequests.cs
@@ -1,11 +1,24 @@
 using Sift;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Test.Integration.Net7.CustomEvents
 {
     public class DecisionsRequests
     {
-        
+        [Fact]
+        public void IntegrationTest_GetDecisionStatusRequest()
+        {
+            var sift = new Client("ccd68efbe25809bc");
+            GetDecisionStatusRequest getDecisionStatusRequest = new GetDecisionStatusRequest
+            {
+                AccountId = "5f053f004025ca08a187fad3",
+                UserId = "haneeshv@exalture.com"
+            };
+
+            GetDecisionStatusResponse res = sift.SendAsync(getDecisionStatusRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+        }
     }
 }
