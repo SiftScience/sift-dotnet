1cbc81a5539987991151d2c1fdd82efb4bfbe3f5
binish-exalture <binishb@exalture.com>
2023-09-14 17:28:17 +0530

    Added IntegrationTest for UnLabelRequest (.Net7)

--- Test.Integration.Net7/CustomEvents/Labels.cs
+++ Test.Integration.Net7/CustomEvents/Labels.cs
@@ -29,18 +29,14 @@ namespace Test.Integration.Net7.CustomEvents
         public void IntegrationTest_UnLabelRequest()
         {
             var sift = new Client("ccd68efbe25809bc");
-            LabelRequest labelRequest = new LabelRequest
+            UnlabelRequest unlabelRequest = new UnlabelRequest
             {
                 UserId = "haneeshv@exalture.com",
                 ApiKey = "ccd68efbe25809bc",
-                IsBad = true,
-                AbuseType = "payment_abuse",
-                Description = "The user was testing cards repeatedly for a valid card",
-                Source = "manual review",
-                Analyst = "someone@your-site.com"
+                AbuseType = "payment_abuse"
             };
 
-            SiftResponse labelResponse = sift.SendAsync(labelRequest).Result;
+            SiftResponse labelResponse = sift.SendAsync(unlabelRequest).Result;
             Assert.Equal("OK", labelResponse.ErrorMessage);
         }
 
