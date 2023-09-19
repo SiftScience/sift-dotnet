d6c87d7ae1a124a362d7f268e4513e9ad482ab7a
binish-exalture <binishb@exalture.com>
2023-09-20 01:45:52 +0530

    Completed VerificationCheck using configuration

--- Test.Integration.NetFx48/VerificationAPI/VerificationsRequests.cs
+++ Test.Integration.NetFx48/VerificationAPI/VerificationsRequests.cs
@@ -78,14 +78,14 @@ namespace Test.Integration.NetFx48.VerificationAPI
             Assert.Equal("OK", verificationReSendResponse.ErrorMessage);
         }
 
-        //[Theory]
-        //[InlineData(990941)]
+        [Theory]
+        [InlineData(990941)]
         public void IntegrationTest_VerificationCheck(int code)
         {
-            var sift = new Client("ccd68efbe25809bc:");
+            var sift = new Client(environmentVariable.ApiKey + ":");
             VerificationCheckRequest verificationCheckRequest = new VerificationCheckRequest
             {
-                UserId = "binishb@exalture.com",
+                UserId = environmentVariable.UserId,
                 Code = code,
                 VerifiedEvent = "$login"
             };
