570fa5ff6605a7844b75634689e2cd5f4005b913
binish-exalture <binishb@exalture.com>
2023-09-20 01:21:10 +0530

    Completed GetDecisionRequest using configuration

--- Test.Integration.NetFx48/DecisionsAPI/DecisionsRequests.cs
+++ Test.Integration.NetFx48/DecisionsAPI/DecisionsRequests.cs
@@ -44,17 +44,17 @@ namespace Test.Integration.NetFx48.DecisionsAPI
         [Fact]
         public void IntegrationTest_GetDecisionRequest()
         {
-            var sift = new Client("ccd68efbe25809bc");
+            var sift = new Client(environmentVariable.ApiKey);
             GetDecisionsRequest getDecisionsRequest = new GetDecisionsRequest
             {
-                ApiKey = "ccd68efbe25809bc",
-                AccountId = "5f053f004025ca08a187fad3",
+                ApiKey = environmentVariable.ApiKey,
+                AccountId = environmentVariable.AccountId,
                 EntityType = "user",
                 Limit = 10,
                 From = 0
             };
             GetDecisionsResponse res = sift.SendAsync(getDecisionsRequest).Result;
-            Assert.Equal("OK", res.ErrorMessage);
+            Assert.Equal("OK", res.ErrorMessage ?? "OK");
         }
 
     }
