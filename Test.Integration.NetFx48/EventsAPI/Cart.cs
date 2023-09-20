e66de0db1adaf499cb5ad5c9c7b8e9be55d459e5
binish-exalture <binishb@exalture.com>
2023-09-20 19:56:26 +0530

    Completed RemoveItemFromCart using configuration

--- Test.Integration.NetFx48/EventsAPI/Cart.cs
+++ Test.Integration.NetFx48/EventsAPI/Cart.cs
@@ -11,7 +11,7 @@ namespace Test.Integration.NetFx48.EventsAPI
         [Fact]
         public void AddItemToCartEvent()
         {
-            var sift = new Client("ccd68efbe25809bc");
+            var sift = new Client(environmentVariable.ApiKey);
             var addItemToCart = new AddItemToCart
             {
                 user_id = environmentVariable.user_id,
@@ -57,15 +57,14 @@ namespace Test.Integration.NetFx48.EventsAPI
         [Fact]
         public void RemoveItemFromCart()
         {
-            var sift = new Client("ccd68efbe25809bc");
-            var sessionId = "sessionId";
+            var sift = new Client(environmentVariable.ApiKey);
             var removeItemFromCart = new RemoveItemFromCart
             {
-                user_id = "billy_jones_301",
-                session_id = "gigtleqddo84l8cm15qe4il",
+                user_id = environmentVariable.user_id,
+                session_id = environmentVariable.session_id,
                 item = new Item()
                 {
-                    item_id = "B004834GQO",
+                    item_id = environmentVariable.item_id,
                     product_title = "The Slanket Blanket-Texas Tea",
                     price = 39990000,
                     currency_code = "USD",
