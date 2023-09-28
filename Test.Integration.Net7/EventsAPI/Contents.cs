62ccd1d378877630cffcaa9e6b1790cc77c9f046
binish-exalture <binishb@exalture.com>
2023-09-28 18:04:29 +0530

    ContentStatus changed to Contents

--- Test.Integration.Net7/EventsAPI/Contents.cs
+++ Test.Integration.Net7/EventsAPI/Contents.cs
@@ -449,5 +449,35 @@ namespace Test.Integration.Net7.EventsAPI
             EventResponse res = sift.SendAsync(eventRequest).Result;
             Assert.Equal("OK", res.ErrorMessage);
         }
+
+        [Fact]
+        public void IntegrationTest_ContentStatus()
+        {
+            var sift = new Client(environmentVariable.ApiKey);
+            var contentStatus = new ContentStatus
+            {
+                user_id = environmentVariable.user_id,
+                session_id = environmentVariable.session_id,
+                content_id = environmentVariable.content_id,
+                status = "$paused",
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_country = "US",
+                site_domain = "sift.com",
+                verification_phone_number = "+123456789012"
+            };
+
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = contentStatus
+            };
+            EventResponse res = sift.SendAsync(eventRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+        }
     }
 }
