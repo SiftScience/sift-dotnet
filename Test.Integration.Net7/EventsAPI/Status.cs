914ec50e6adcfd90ffe300ec56af645b82fe4742
binish-exalture <binishb@exalture.com>
2023-09-28 18:10:32 +0530

    OrderStatus changed to Order

--- Test.Integration.Net7/EventsAPI/Status.cs
+++ Test.Integration.Net7/EventsAPI/Status.cs
@@ -10,36 +10,6 @@ namespace Test.Integration.Net7.EventsAPI
         private readonly EnvironmentVariable environmentVariable = new();
         
 
-        [Fact]
-        public void IntegrationTest_OrderStatus()
-        {
-            var sift = new Client(environmentVariable.ApiKey);
-            var orderStatus = new OrderStatus
-            {
-                user_id = environmentVariable.user_id,
-                order_id = environmentVariable.order_id,
-                order_status = "$canceled",
-                reason = "$payment_risk",
-                source = "$manual_review",
-                analyst = "someone@your-site.com",
-                webhook_id = environmentVariable.webhook_id,
-                description = "Canceling because multiple fraudulent users on device",
-                browser = new Browser
-                {
-                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-                    accept_language = "en-US",
-                    content_language = "en-GB"
-                },
-                brand_name = "sift",
-                site_country = "US",
-                site_domain = "sift.com"
-            };
-            EventRequest eventRequest = new EventRequest()
-            {
-                Event = orderStatus
-            };
-            EventResponse res = sift.SendAsync(eventRequest).Result;
-            Assert.Equal("OK", res.ErrorMessage);
-        }
+        
     }
 }
