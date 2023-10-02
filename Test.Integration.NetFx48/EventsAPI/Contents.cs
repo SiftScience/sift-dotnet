ba9cfb79ff64398400580b44697c9002c4373e6b
binish-exalture <binishb@exalture.com>
2023-10-03 02:14:41 +0530

    Flags & Status tests added to Content

--- Test.Integration.NetFx48/EventsAPI/Contents.cs
+++ Test.Integration.NetFx48/EventsAPI/Contents.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace Test.Integration.NetFx48.EventsAPI
 {
-    public class CreateContents
+    public class Contents
     {
         private readonly EnvironmentVariable environmentVariable = new EnvironmentVariable();
         [Fact]
@@ -429,6 +429,59 @@ namespace Test.Integration.NetFx48.EventsAPI
             EventRequest eventRequest = new EventRequest()
             {
                 Event = createContent
+            };
+            EventResponse res = sift.SendAsync(eventRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+            Assert.Equal("0", res.Status.ToString());
+        }
+
+        [Fact]
+        public void ContentStatus()
+        {
+            var sift = new Client(environmentVariable.ApiKey);
+            var contentStatus = new ContentStatus
+            {
+                user_id = environmentVariable.user_id,
+                session_id = environmentVariable.session_id,
+                content_id = environmentVariable.content_id,
+                status = "$paused",
+                browser = new Browser
+                {
+                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+                    accept_language = "en-US",
+                    content_language = "en-GB"
+                },
+                brand_name = "sift",
+                site_country = "US",
+                site_domain = "sift.com",
+                verification_phone_number = "+123456789012"
+            };
+
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = contentStatus
+            };
+            EventResponse res = sift.SendAsync(eventRequest).Result;
+            Assert.Equal("OK", res.ErrorMessage);
+            Assert.Equal("0", res.Status.ToString());
+        }
+
+        [Fact]
+        public void FlagContentTest()
+        {
+            var sift = new Client(environmentVariable.ApiKey);
+            var flagContent = new FlagContent
+            {
+                user_id = environmentVariable.user_id,
+                session_id = environmentVariable.session_id,
+                content_id = environmentVariable.content_id,
+                flagged_by = environmentVariable.flagged_by,
+                reason = "$toxic",
+                verification_phone_number = "+123456789012"
+            };
+            EventRequest eventRequest = new EventRequest()
+            {
+                Event = flagContent
             };
             EventResponse res = sift.SendAsync(eventRequest).Result;
             Assert.Equal("OK", res.ErrorMessage);
