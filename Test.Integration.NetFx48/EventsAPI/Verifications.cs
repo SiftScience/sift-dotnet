987d9eda58495041de32dccd9c4b90963fae2023
binish-exalture <binishb@exalture.com>
2023-11-11 00:25:47 +0530

    Verifications status checked

--- Test.Integration.NetFx48/EventsAPI/Verifications.cs
+++ Test.Integration.NetFx48/EventsAPI/Verifications.cs
@@ -46,7 +46,6 @@ namespace Test.Integration.NetFx48.EventsAPI
                 Event = verification
             };
             EventResponse res = sift.SendAsync(eventRequest).Result;
-            Assert.Equal("OK", res.ErrorMessage);
             Assert.Equal("0", res.Status.ToString());
         }
     }
