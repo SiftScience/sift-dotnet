498bf8971995bd9f09f0b94ef9afbfdbac6315e9
binish-exalture <binishb@exalture.com>
2023-11-10 16:19:23 +0530

    Sesseions added through constructor

--- Test.Integration.NetFx48/EventsAPI/Sessions.cs
+++ Test.Integration.NetFx48/EventsAPI/Sessions.cs
@@ -7,14 +7,23 @@ namespace Test.Integration.NetFx48.EventsAPI
     public class Sessions
     {
         private readonly EnvironmentVariable environmentVariable = new EnvironmentVariable();
+        private readonly string ApiKey;
+        private readonly string UserId;
+        private readonly string SessionId;
+        public Sessions()
+        {
+            ApiKey = environmentVariable.ApiKey;
+            UserId = environmentVariable.user_id;
+            SessionId = environmentVariable.session_id;
+        }
         [Fact]
         public void LinkSessionToUserTest()
         {
-            var sift = new Client(environmentVariable.ApiKey);
+            var sift = new Client(ApiKey);
             var linkSessionToUser = new LinkSessionToUser
             {
-                user_id = environmentVariable.user_id,
-                session_id = environmentVariable.session_id
+                user_id = UserId,
+                session_id = SessionId
             };
             EventRequest eventRequest = new EventRequest()
             {
