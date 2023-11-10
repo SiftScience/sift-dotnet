c6683686ca6c7f87633effaf3b659f6db118ed41
binish-exalture <binishb@exalture.com>
2023-11-10 16:39:29 +0530

    WorkflowStatuses added through constructor

--- Test.Integration.NetFx48/WorkflowsAPI/WorkflowStatuses.cs
+++ Test.Integration.NetFx48/WorkflowsAPI/WorkflowStatuses.cs
@@ -8,19 +8,27 @@ namespace Test.Integration.NetFx48.WorkflowsAPI
     public class WorkflowStatuses
     {
         private readonly EnvironmentVariable environmentVariable = new EnvironmentVariable();
+        private readonly string ApiKey;
+        private readonly string AccountId;
+        private readonly string WorkflowRunId;
+        public WorkflowStatuses()
+        {
+            ApiKey = environmentVariable.ApiKey;
+            AccountId = environmentVariable.AccountId;
+            WorkflowRunId = environmentVariable.WorkflowRunId;
+        }
         [Fact]
         public void WorkflowStatusesTest()
         {
-            var sift = new Client(environmentVariable.ApiKey);
+            var sift = new Client(ApiKey);
             WorkflowStatusRequest workflowStatusRequest = new WorkflowStatusRequest
             {
-                ApiKey = environmentVariable.ApiKey,
-                AccountId = environmentVariable.AccountId,
-                WorkflowRunId = environmentVariable.WorkflowRunId
+                ApiKey = ApiKey,
+                AccountId = AccountId,
+                WorkflowRunId = WorkflowRunId
             };
             WorkflowStatusResponse workFlowStatusResponse = sift.SendAsync(workflowStatusRequest).Result;
             Assert.Equal("OK", workFlowStatusResponse.ErrorMessage);
         }
-
     }
 }
