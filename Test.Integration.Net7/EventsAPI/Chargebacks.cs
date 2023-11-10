2f72ba60fdc9d45a7ca862fcd9b8ea3c48e10128
binish-exalture <binishb@exalture.com>
2023-11-10 14:26:41 +0530

    Chargeback event added through constructor

--- Test.Integration.Net7/EventsAPI/Chargebacks.cs
+++ Test.Integration.Net7/EventsAPI/Chargebacks.cs
@@ -23,17 +23,17 @@ namespace Test.Integration.Net7.EventsAPI
         [Fact]
         public void ChargebackTest()
         {
-            var sift = new Client(environmentVariable.ApiKey);
+            var sift = new Client(ApiKey);
             var chargeback = new Chargeback
             {
-                user_id = environmentVariable.user_id,
-                order_id = environmentVariable.order_id,
-                transaction_id = environmentVariable.transaction_id,
+                user_id = UserId,
+                order_id = OrderId,
+                transaction_id = TransactionId,
                 chargeback_state = "$lost",
                 chargeback_reason = "$duplicate",
                 merchant_profile = new MerchantProfile()
                 {
-                    merchant_id = environmentVariable.merchant_id,
+                    merchant_id = MerchantId,
                     merchant_category_code = "1234",
                     merchant_name = "Dream Company",
                     merchant_address = new Address()
