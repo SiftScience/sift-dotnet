16400d1e36588eb400b90eeae7f2cdc1c8049042
binish-exalture <binishb@exalture.com>
2023-11-10 19:58:55 +0530

    checked status=0 for verifications test

--- Test.Integration.Net7/EventsAPI/Verifications.cs
+++ Test.Integration.Net7/EventsAPI/Verifications.cs
@@ -48,7 +48,6 @@ namespace Test.Integration.Net7.EventsAPI
                 Event = verification
             };
             EventResponse res = sift.SendAsync(eventRequest).Result;
-            Assert.Equal("OK", res.ErrorMessage);
             Assert.Equal("0", res.Status.ToString());
         }
     }
