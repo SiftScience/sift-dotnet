25ff357f775f664e97184a918545bbf80468695c
binish-exalture <binishb@exalture.com>
2023-11-10 13:33:46 +0530

    Verifications added through constructor

--- Test.Integration.Net7/DecisionsAPI/DecisionsRequests.cs
+++ Test.Integration.Net7/DecisionsAPI/DecisionsRequests.cs
@@ -7,15 +7,24 @@ namespace Test.Integration.Net7.DecisionsAPI
     public class DecisionsRequests
     {
         private readonly EnvironmentVariable environmentVariable = new();
+        private readonly string ApiKey;
+        private readonly string AccountId;
+        private readonly string UserId;
+        public DecisionsRequests()
+        {
+            ApiKey = environmentVariable.ApiKey;
+            AccountId = environmentVariable.AccountId;
+            UserId = environmentVariable.user_id;
+        }
 
         //[Fact]
         public void GetDecisionStatusRequest()
         {
-            var sift = new Client(environmentVariable.ApiKey);
+            var sift = new Client(ApiKey);
             GetDecisionStatusRequest getDecisionStatusRequest = new GetDecisionStatusRequest
             {
-                AccountId = environmentVariable.AccountId,
-                UserId = environmentVariable.UserId
+                AccountId = AccountId,
+                UserId = UserId
             };
             GetDecisionStatusResponse res = sift.SendAsync(getDecisionStatusRequest).Result;
             Assert.Equal("OK", res.ErrorMessage ?? "OK");
@@ -24,17 +33,17 @@ namespace Test.Integration.Net7.DecisionsAPI
         //[Fact]
         public void ApplyDecisionRequest()
         {
-            var sift = new Client(environmentVariable.ApiKey);
+            var sift = new Client(ApiKey);
             ApplyDecisionRequest applyDecisionRequest = new ApplyDecisionRequest
             {
-                ApiKey = environmentVariable.ApiKey,
+                ApiKey = ApiKey,
                 DecisionId = "block_user_payment_abuse",
                 Source = "MANUAL_REVIEW",
                 Analyst = "analyst@example.com",
                 Time = 1231234123,
                 Description = "compromised account reported to customer service",
-                AccountId = environmentVariable.AccountId,
-                UserId = environmentVariable.UserId
+                AccountId = AccountId,
+                UserId = UserId
             };
             ApplyDecisionResponse res = sift.SendAsync(applyDecisionRequest).Result;
             Assert.Equal("OK", res.ErrorMessage);
@@ -43,11 +52,11 @@ namespace Test.Integration.Net7.DecisionsAPI
         //[Fact]
         public void GetDecisionRequest()
         {
-            var sift = new Client(environmentVariable.ApiKey);
+            var sift = new Client(ApiKey);
             GetDecisionsRequest getDecisionsRequest = new GetDecisionsRequest
             {
-                ApiKey = environmentVariable.ApiKey,
-                AccountId = environmentVariable.AccountId,
+                ApiKey = ApiKey,
+                AccountId = AccountId,
                 EntityType = "user",
                 Limit = 10,
                 From = 0
